d3d547b62600c62768f74591ee4fcba6297bb9ca
zerox123s <joseakvs16@gmail.com>
2025-11-29 23:53:33 -0500

    nueva

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Analytics } from "@vercel/analytics/next"
 import './index.css'
 import App from './App.jsx'
 
